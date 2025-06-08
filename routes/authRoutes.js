049c3c6b058007e4d6bc1c6895127efb6a62deec
lakshyarohila <lakshyarohila99@gmail.com>
2025-06-08 22:58:46 +0530

    change in google  Strategy callback url

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -84,7 +84,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: "https://vigyaana-server.onrender.com/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
