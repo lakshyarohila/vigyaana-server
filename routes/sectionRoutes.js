a790acbfc83e144ecefe74ba01accad55a55a30e
lakshyarohila <lakshyarohila99@gmail.com>
2025-06-25 22:23:49 +0530

    updated section upadeted

--- routes/sectionRoutes.js
+++ routes/sectionRoutes.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
-
+const protect = require('../middleware/auth');
+const { isInstructor } = require('../middleware/roles');
+const upload = require('../config/multer');
 const {
   addSection,
   getCourseSections,
-  createLiveSession,
-  getLiveSessions
 } = require('../controllers/sectionController');
 
-const protect = require('../middleware/auth');
-
-// For recorded sections
-router.post('/', protect, upload.single('video'), addSection);
-router.get('/:courseId', protect, getCourseSections);
-
-// For live sessions
-router.post('/live/:courseId', protect, createLiveSession);
-router.get('/live/:courseId', protect, getLiveSessions);
+router.post('/', protect, isInstructor, upload.single('video'), addSection);
+router.get('/:courseId', getCourseSections);
 
 module.exports = router;
